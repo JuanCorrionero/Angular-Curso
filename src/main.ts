44e8c6b954fa1020015165b3e29057999cf33cb6
JuanCorrionero <52964596+JuanCorrionero@users.noreply.github.com>
2023-07-11 08:38:52 -0300

    Clase 4 - Funciones y Homework

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css';
 
 // import './topics/02-object-interface';
-import './topics/03-functions';
+// import './topics/03-functions';
+import './topics/04-homework-types';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
